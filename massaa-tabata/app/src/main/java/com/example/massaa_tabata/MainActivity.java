2adbfecb9b9e59bb44dc25a24ae0a38b078c7292
Alexis Massa <alexis.massa351@gmail.com>
2021-11-08 15:53:54 +0100

    app

---
+++ massaa-tabata/app/src/main/java/com/example/massaa_tabata/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.massaa_tabata;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.massaa_tabata.data.Compteur;
+import com.example.massaa_tabata.data.OnUpdateListener;
+
+public class MainActivity extends AppCompatActivity  implements OnUpdateListener {
+
+    // VIEW
+    private TextView timerValue;
+
+    // DATA
+    private Compteur compteur;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Récupérer la view
+        timerValue = (TextView) findViewById(R.id.timerValue);
+
+        // Initialiser l'objet Compteur
+        compteur = new Compteur();
+
+        // Abonner l'activité au compteur pour "suivre" les événements
+        compteur.addOnUpdateListener(this);
+
+        // Mise à jour graphique
+        miseAJour();
+    }
+
+    // Lancer le compteur
+    public void onStart(View view) {
+        compteur.start();
+    }
+
+    // Mettre en pause le compteur
+    public void onPause(View view) {
+        compteur.pause();
+    }
+
+
+    // Remettre à zéro le compteur
+    public void onReset(View view) {
+        compteur.reset();
+    }
+
+    // Mise à jour graphique
+    private void miseAJour() {
+
+        // Affichage des informations du compteur
+        timerValue.setText("" + compteur.getMinutes() + ":"
+                + String.format("%02d", compteur.getSecondes()) + ":"
+                + String.format("%03d", compteur.getMillisecondes()));
+
+    }
+
+    /**
+     * Méthode appelée à chaque update du compteur (l'activité est abonnée au compteur)
+     *
+     */
+    @Override
+    public void onUpdate() {
+        miseAJour();
+    }
+}
